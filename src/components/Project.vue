bab32c36d400785febeff431b06ea52373c7c181
Ed Lu <edlu@me.com>
2024-10-09 08:01:51 -0700

    project hover tuning

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -9,10 +9,11 @@ defineProps({
   role: String,
   platform: String
 });
+
 </script>
 
 <template>
-<section class="project padding-4x-tablet" :style="{ backgroundImage: `url('${imgUrl}')` }">
+<section class="project padding-4x-tablet" tabindex="1">
   <div class="project__metadata">
     <h2>{{ title }}</h2>
     <table>
@@ -36,12 +37,14 @@ defineProps({
       </tbody>
     </table>
   </div>
-  <div class="overlay"></div>
+  <div class="project__scrim"></div>
+  <div class="project__image" :style="{ backgroundImage: `url('${imgUrl}')` }"></div>
 </section>
 
 </template>
 
 <style scoped>
+
 .project {
   position: relative;
   display: flex;
@@ -50,25 +53,63 @@ defineProps({
   align-items: flex-start;
   padding: var(--spacing-2x);
   width: 100%;
-  height: 465px;
-  background-position: center;
-  background-size: 100%;
+  height: 550px;
+  overflow: hidden;
   transition: all var(--transition-slow);
 
   @media (max-width: 576px) {
     height: 300px;
+    filter: saturate(1);
   }
 
-  &:hover {
+  &:hover, &:focus, &:active {
     cursor: pointer;
     background-size: 102%;
-    .overlay {
-      opacity: 0.75;
-    }
+    outline: none;
+    
     .project__metadata {
       color: var(--color-text-inverse);
       transform: translateY(-8px);
     }
+
+    .project__scrim {
+      opacity: 0.35; 
+    }
+
+    .project__image {
+      transform: scale(1.05);
+      filter: saturate(1);
+    }
+  }
+}
+
+.project__image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  filter: saturate(0.5);
+  transition: all var(--transition-slow);
+}
+
+.project__scrim {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(195deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.6) 50%, rgba(0, 0, 0, 0.75) 70%, rgba(0, 0, 0, 0.95) 100%);
+  z-index:1;
+  transition: opacity var(--transition-slow);
+  opacity: 1;
 }
 
 .project__metadata {
@@ -77,37 +118,25 @@ defineProps({
   font-size: var(--size-s);
   z-index: 2;
   transition: all var(--transition-slow);
-  }
-
+  
   h2 {
     color: var(--color-text-inverse);
     margin-bottom: var(--spacing-half);
   }
+
+  td {
+    padding: var(--spacing-quarter);
+  }
+
+  .label {
+    color: var(--color-text-inverse-dim);
+    padding-right: var(--spacing-3x);
+    width: 100px;
+  }
+
+  .value {
+    color: var(--color-text-inverse);
+  }
 }
 
-td {
-  padding: var(--spacing-quarter);
-}
-
-.label {
-  color: var(--color-text-inverse-dim);
-  padding-right: var(--spacing-3x);
-  width: 100px;
-}
-
-.value {
-  color: var(--color-text-inverse);
-}
-
-.overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.7) 50%, rgba(0, 0, 0, 0.9) 70%, rgba(0, 0, 0, 1) 100%);
-  opacity: 0.9;
-  z-index:1;
-  transition: opacity var(--transition-medium);
-}
 </style>
