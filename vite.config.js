ba0d84213632d7ee18a55371e6e0e38af966738a
Ed Lu <edlu@me.com>
2024-10-18 14:08:08 -0700

    set up files to publish to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['Project'].includes(tag),
-        }
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  css: {
-    // CSS preprocessing configuration
-    preprocessorOptions: {
-      // Add any preprocessor options here if needed
-    },
-  },
-  build: {
-    // Ensure CSS is extracted to a separate file
-    cssCodeSplit: true,
-    // Specify the output directory for the build
-    outDir: 'dist',
-    // Ensure assets are copied to the dist folder
-    assetsDir: 'assets',
-    rollupOptions: {
-      // Specify the entry point for your app
-      input: path.resolve(__dirname, 'index.html'),
-      output: {
-        // Specify how to name the output files
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  }
+  plugins: [vue()],
+  base: '/portfolio/', // Replace with your actual repository name
 })
