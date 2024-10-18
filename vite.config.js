61473bf95355733cf3f98acc6bccb4ba9bdab632
Ed Lu <edlu@me.com>
2024-10-18 16:13:52 -0700

    path issues

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/portfolio/', // Replace with your actual repository name
+  base: '/portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
+
